b4167aa3c93564469d926a81c1819324aad44ea6
Bestun Gardi <bestun95@yahoo.com>
2018-08-09 07:52:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,57 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [9,9,9,1]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+// function init() {
+// let index = 0;
+ 
+//   document.body.addEventListener('keydown', function(e) {
+//     const key = parseInt(e.which || e.detail);
+ 
+//   if (key === alphabet[index]) {
+//     index++;
+ 
+//     if (index === alphabet.length) {
+//       alert("Hurray!");
+ 
+//       index = 0;
+//     }
+//   } else {
+//     index = 0;
+//   }
+//   });}
+  
+  
+  function init() {
+  let index = 0;
 
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+// function init()
+// {
+// let i = 0;
+// document.body.addeventListener("keydown", function (key){
+  
+//   if(key.which === code[i]){
+//     i++;
+//   }
+//   console.log(i, "befoore 2nd if")
+//   if(i === code.length-1){
+//     alert("You did it")
+//   }
+//   console.log(i,"after second if")
+// });}
